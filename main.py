4330e024d210772df3feddf875ab44a3b454a6b8
abdulAziz-Al-ILM <abdulazizibnibrohim636@gmail.com>
2025-11-24 23:15:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,8 @@ async def cmd_start(message: Message):
         f"Assalomu alaykum, {message.from_user.full_name}!\n\n"
         "Men fayllarni qayta nomlab beruvchi bepul botman. "
         "Menga istalgan hujjat, video yoki audio fayl yuboring.\n\n"
-        "Agar reklama bo'yicha savollaringiz bo'lsa, menyudagi tugmadan foydalaning.",
+        "Agar reklama bo'yicha savollaringiz bo'lsa, menyudagi tugmadan foydalaning. \n\n"
+        "Foydalanish qoidalari (ToU) bilan tanishing: https://t.me/Atomic_Online_Services/5",
         reply_markup=main_keyboard(message.from_user.id)
     )
 
@@ -96,7 +97,7 @@ async def help_handler(message: Message):
         "2. Men sizdan yangi nom so'rayman.\n"
         "3. Yangi nomni yozasiz (kengaytmani yozish shart emas, masalan: `.pdf`, `.mp4` ni o'zim qo'yaman).\n"
         "4. Men faylni o'zgartirib sizga qaytaraman.\n\n"
-        "⚠️ <i>Eslatma: Fayl nomida / \\ : * ? \" < > | belgilaridan foydalanmang! </i>"
+        "⚠️ <i>Eslatma: Fayl nomida / \\ : * ? \" < > | belgilaridan foydalanmang! </i>",
     )
     await message.answer(text, parse_mode="HTML")
 
